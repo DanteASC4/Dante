15bee810c44b2ca68f0f61a699cf4d496a3f6ca6
DanteASC4 <zexovon@gmail.com>
2017-07-21 15:34:11 -0400

    New files, image is not mine, again, just one part missing from being fully complete

---
+++ Dante-spaceinvaders/spaceinvader.js
@@ -0,0 +1,120 @@
+function setup() {
+  createCanvas(1300, 600);
+  background('black');
+
+}
+
+var xPos = 40;
+var xPos1 = 80;
+var xPos2 = 120;
+var xPos3 = 160;
+var xPos4 = 200;
+
+var v = 4;
+var w = 4;
+var u = 4;
+var g = 4;
+var z = 4;
+
+var mainCharacter;
+var x = 100;
+var y = 100;
+var bullets = [];
+
+var hit = false;
+
+function draw() {
+  background('black');
+
+  if (xPos < 25 || xPos > 1250) u = -u;
+  xPos += u;
+
+  if (xPos1 < 25 || xPos1 > 1250) g = -g;
+  xPos1 += g;
+
+  if (xPos2 < 25 || xPos2 > 1250) z = -z;
+  xPos2 += z;
+
+  if (xPos3 < 25 || xPos3 > 1250) w = -w;
+  xPos3 += w;
+
+  if (xPos4 < 25 || xPos4 > 1250) v = -v;
+  xPos4 += v;
+
+  ellipseMode(CENTER);
+
+
+
+console.log(bullets);
+console.log(Bullet(x, y));
+
+  ellipse(xPos, 50, 20, 20);
+  if((Bullet(this.x) >= xPos + 50 && Bullet(this.x) <= xPos - 50) && Bullet(this.y) >= 20){
+    fill('black')
+  }
+  ellipse(xPos1, 50, 20, 20);
+  ellipse(xPos2, 50, 20, 20);
+  ellipse(xPos3, 50, 20, 20);
+  ellipse(xPos4, 50, 20, 20);
+
+
+
+
+
+  function Shooter() {
+    this.x = width / 2;
+  }
+
+  var mainCharacter = new Shooter();
+
+
+
+  for (var i = 0; i < bullets.length; i++) {
+    bullets[i].show();
+    bullets[i].move();
+
+  }
+
+
+  if (keyIsDown(LEFT_ARROW)) {
+    x -= 10
+  }
+  if (keyIsDown(RIGHT_ARROW)) {
+    x += 10
+  }
+  if (x > 1299) {
+    x = 0
+  } else if (x <= 1) {
+    x = 1298
+  }
+  if (keyIsDown(UP_ARROW)) {
+    bullets.push(new Bullet(x, height));
+  }
+
+
+
+
+  rect(x, 500, 20, 100);
+  fill("white");
+
+
+
+
+  function Bullet(x, y) {
+    this.x = x;
+    this.y = y;
+    this.r = 21;
+
+
+    this.show = function() {
+      // background('black')
+      rect(this.x, this.y, 10, 25)
+    }
+
+    this.move = function() {
+      this.y -= 20;
+    }
+
+  }
+
+}
--- dante-pokemonBattle/pokemonbattle.js
+++ dante-pokemonBattle/pokemonbattle.js
@@ -1,75 +1,19 @@
+var img;
+
+
 function setup(){
+  img = loadImage('http://i.imgur.com/sfKY3Tj.png');
   createCanvas(1360, 600);
-  background(255, 255, 255);
   textSize(20);
 
-  var pokeHp = 0;
 
-  var pokeView = prompt("Which do you want to use to battle?? zoroark, deoxys or mawile? Type your choice exactly as shown.");
 
-  if(pokeView == "zoroark"){
-    stats(zoroark);
-  }else if(pokeView == "deoxys"){
-    stats(deoxys);
-  }else{
-    stats(mawile);
-  }
-
-  function stats(userChoice){
-    for (var i=0; i<6; i++) {
-  	   text(userChoice[i], 100, 200 + (30 * i), 500, 500);
-     }
-  }
-
-  function pokeBattle(){
-    if(pokeView == "zoroark"){
-      pokeHp = 60;
-    }
-    else if(pokeView == "deoxys"){
-      pokeHp = 50;
-    }
-    else {
-      pokeHp = 50;
-    }
-  }
 
 }
-function mousePressed(){
-    var thunderboltbutton1 = dist(mouseX, mouseY, 95,500);
-    var thunderboltbutton2 = dist(mouseX,mouseY,210,550);
+function draw(){
+  background(img);
 
-   if (thunderboltbutton1< 100 && thunderboltbutton2< 100){
-        fill(0);
-        textSize(20);
-        text(pokeView + " attacked the enemy for 70 damage!",50,300)
-    }
-    if (opponentattack<pokeHp){
-        fill(0);
-        textSize(20);
-        text(pokeView + " has recieved 40 damage and now has " + (pokeHp - opponentattack) + " health left.",600,300);
-        pokeHp = pokeHp - opponentattack
-   }else if(opponentattack>pokeHp){
-        fill(0);
-        textSize(20);
-        text(pokeView + "has recieved fatal damage and has now fainted.")
-    }
-    fill(0)
-    textSize(20)
-    text( pokeView + ": " + pokeHp +"/60",200,400)
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -89,13 +33,4 @@ var deoxys = [
   ["Def: 50"],
   ["Atk: 150"],
   ["Legendary: Yup"],
-];
-
-var mawile = [
-  ["Name: Mawile"],
-  ["Type: Fairy, Steel"],
-  ["Hp: 50"],
-  ["Def: 125"],
-  ["Atk: 105"],
-  ["Legendary: Nope"],
 ];
